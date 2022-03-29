c596152fce46fdbb8dd7027c6065194c5414eeb7
amino19 <anshumaankrprasad76@gmail.com>
2022-03-29 22:08:48 +0530

    🛠️  | initial commit
    Signed-off-by: amino19 <anshumaankrprasad76@gmail.com>

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,9 +3,9 @@ import "./Header.scss";
 
 export default function Header() {
   return (
-    <Header className="header-content">
+    <div className="header-content">
       <h2>aminos</h2>
       <h2>aminos</h2>
-    </Header>
+    </div>
   );
 }
