64b372baf65d583d7ed2dadc72cae93355aa8e31
amino19 <anshumaankrprasad76@gmail.com>
2022-03-30 03:39:54 +0530

    🛠️  | initial commit
    Signed-off-by: amino19 <anshumaankrprasad76@gmail.com>

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,20 +7,20 @@ export default function About() {
     <>
       <div className="about">
         <a
-          className="bg_links social portfolio"
-          href="https://www.aminos.vercel.app"
+          className="bg_links social twitter"
+          href="https://www.twitter.com/aminoxix"
         >
           <span className="icon"></span>
         </a>
         <a
-          className="bg_links social dribbble"
-          href="https://github.com/amino19"
+          className="bg_links social instagram"
+          href="https://instagram.com/aminos.ly"
         >
           <span className="icon"></span>
         </a>
         <a
           className="bg_links social linkedin"
-          href="https://www.linkedin.com/in/amino19/"
+          href="https://www.linkedin.com/in/amino19"
         >
           <span className="icon"></span>
         </a>
@@ -33,21 +33,24 @@ export default function About() {
             <i className="material-icons md-36">face</i>
           </div>
           <p className="title">Profile</p>
-          <p className="text">Click to see or edit your profile page.</p>
+          <p className="text">Click here to know more about my journey</p> 
+          {/* polywork */}
         </div>
         <div className="card-fav">
           <div className="icon">
             <i className="material-icons md-36">favorite_border</i>
           </div>
-          <p className="title">Favourites</p>
-          <p className="text">Check all your favourites in one place.</p>
+          <p className="title">Projects</p>
+          <p className="text">Open Source projects showcased here</p>
+          {/* github */}
         </div>
         <div className="card-about">
           <div className="icon">
             <i className="material-icons md-36">alternate_email</i>
           </div>
           <p className="title">Contacts</p>
-          <p className="text">Add or change your contacts and links.</p>
+          <p className="text">Feel free to book a time slot for 1:1 meeting</p>
+          {/* calendly */}
         </div>
       </div>
     </>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,9 +3,14 @@ import "./Header.scss";
 
 export default function Header() {
   return (
-    <div className="header-content">
-      <h2>aminos</h2>
-      <h2>aminos</h2>
-    </div>
+    <>
+      <div className="header-content">
+        <h2>Anshumaan Kumar Prasad</h2>
+        <h2>Anshumaan Kumar Prasad</h2>
+      </div>
+      <div>
+        <h3>A Junior at Information Technology & Services</h3>
+      </div>
+    </>
   );
 }
